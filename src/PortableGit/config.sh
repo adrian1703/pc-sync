641c80728436864c5e758d4de1bdd05971857c14
adrian <adrian.kuhn@arcor.de>
2024-12-04 19:43:44 +0100

    config sync

--- src/PortableGit/config.sh
+++ src/PortableGit/config.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-if [ "$1" = "-push" ]; then
+repo_dir="$DEV/projects/ReinstallPc/src/PortableGit/etc/"
+git_dir="$DEV/PortableGit/etc/"
+files=$(find "$repo_dir" -type f | sed -E 's|.*/etc/||')
+
+print_files() {
+  echo "The following files are concerned:"
+  echo "$files"
+}
+
+copy() {
+    local src=$1
+    local dst=$2
+
+    for file in $files; do
+        cp -f "$src$file" "$dst$file"
+    done
+}
+
+# Define subroutine for pushing changes
+push_changes() {
   echo "Pushing changes to config project"
+  copy "$git_dir" "$repo_dir"
+}
 
-elif [ "$1" = "-pull" ]; then
+# Define subroutine for pulling changes
+pull_changes() {
   echo "Pulling changes to local config"
+  copy "$repo_dir" "$git_dir"
+}
 
+# Main script logic
+if [ "$1" = "-push" ]; then
+  print_files
+  push_changes
+elif [ "$1" = "-pull" ]; then
+  print_files
+  pull_changes
 else
   echo "Unknown option, please use -push or -pull"
 fi
