36a5c1fce1eb54516d3d5efaa7cc76102a0052f7
adrian <adrian.kuhn@arcor.de>
2024-12-06 14:44:10 +0100

    adjusted git install

--- src/Git/install_git.ps1
+++ src/Git/install_git.ps1
@@ -1,20 +1,24 @@
+#! /usr/bin/env pwsh
+
 # 1. Define the URL of the Git Bash installer
-$gitBashUrl = "https://github.com/git-for-windows/git/releases/download/v2.47.1.windows.1/PortableGit-2.47.1-64-bit.7z.exe"
-$downloadFolder = "$env:DEV\temp"
+$git_exe='GitBashInstaller.exe'
+$git_dl_link='https://github.com/git-for-windows/git/releases/download/v2.47.1.windows.1/Git-2.47.1-64-bit.exe'
+$downloadFolder = "c:/development/temp"
+$config_file = "./git_options.ini"
 
 # 2. Define the path where the installer will be saved
 if (!(Test-Path $downloadFolder)) {
     New-Item -ItemType Directory -Force -Path $downloadFolder
 }
-$installerPath = "$downloadFolder\GitBashInstaller.exe"
+$installerPath = "$downloadFolder/$git_exe"
 
 # 3. Download the installer
 Write-Host "Downloading Git Bash installer... to $installerPath"
-Invoke-WebRequest -Uri $gitBashUrl -OutFile $installerPath
+Invoke-WebRequest -Uri $git_dl_link -OutFile $installerPath
 
 # 4. Install Git Bash
-#Write-Host "Installing Git Bash..."
-#Start-Process -FilePath $installerPath -Args "/VERYSILENT /NORESTART" -NoNewWindow -Wait
+Write-Host "Installing Git Bash..."
+Start-Process -FilePath $installerPath -Args "/VERYSILENT /NORESTART /NOCANCEL /LOADINF=$config_file"
 #
 ## 5. Clean up the installer
 #Remove-Item -Path $installerPath
