04676b126da4adbd9fce107560fbbf0285126828
adrian <adrian.kuhn@arcor.de>
2025-01-23 18:42:16 +0100

    powershell profile WIP

---
+++ Powershell/profile.ps1
@@ -0,0 +1,6 @@
+Set-Alias ls "ls"
+Set-Alias ll "ls -lah"
+Set-Alias cll "Clear-Host; ls -lah"
+Set-Alias dev "Set-Location $env:DEV"
+Set-Alias proj "Set-Location $env:DEV\projects"
+Set-Alias proj "Set-Location $env:DEV\projects"
