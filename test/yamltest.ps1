05497c2975c6092355cf1ae21241aa847b08b072
adrian <adrian.kuhn@arcor.de>
2025-02-08 17:22:34 +0100

    some experimental tests

--- test/yamltest.ps1
+++ test/yamltest.ps1
@@ -1,9 +1,31 @@
 # Read the YAML file
 $path = Join-Path $env:SETUP_PROJ "src\config.yaml"
 $yamlContent = Get-Content -Raw -Path $path
-
+$yamlContent = [regex]::replace($yamlContent, '\$\{(.*?)\}', {
+    param($match)
+    $envVarName = $match.Groups[1].Value
+    return [System.Environment]::GetEnvironmentVariable($envVarName)
+})
+Write-Host $yamlContent
 # Convert YAML to PowerShell object
 $parsedData = ConvertFrom-Yaml -Yaml $yamlContent
 
 # Display the parsed object
-$parsedData
+Write-Host $parsedData
+
+foreach ($key in $parsedData.Keys)
+{
+    $value = $parsedData[$key]
+    Write-Host "$key : $value"
+}
+
+Write-Host $parsedData.rider.Keys
+
+foreach ($item in $parsedData.schema.stages)
+{
+    Write-Host $item.name
+    Write-Host $item.prio
+}
+Write-Host $parsedData.schema.stages
+$parsedData.schema.stages
+
