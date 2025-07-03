31670a82d7ff5226e9b4d8a243e697c330f962b8
Adrian <adrian.kuhn97@gmail.com>
2025-07-03 20:22:43 +0200

    install nerd mono jetbrains

---
+++ jetbrainsfont.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+#sudo dnf copr enable maveonair/jetbrains-mono-nerd-fonts
+#sudo dnf install jetbrains-mono-nerd-fonts jetbrains-mononl-nerd-fonts
+#fc-cache -fv
+#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
+#unzip JetBrainsMono.zip -d JetBrainsMono
+mkdir -p ~/.local/share/fonts
+cp JetBrainsMono/*.ttf ~/.local/share/fonts/
+fc-cache -fv
+rm -r JetBrainsMono && rm JetBrainsMono.zip
--- readme.md
+++ readme.md
@@ -17,6 +17,11 @@ done
 ## jetbrains toolbox
 https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.6.3.43718.tar.gz
 
+## fonts 
+- jetbrains mono single install; manually
+- cant execute script twice
+- still need to configure the terminal to use it
+
 ## Packages
 
 - some packages are in installSoftware for dnf
