7b5e8fb85d008fda91ba6f17b398d3e8d18c91f1
Adrian <adrian.kuhn97@gmail.com>
2025-07-03 21:11:17 +0200

    add inital wezterm

--- installSoftware.bash
+++ installSoftware.bash
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
 
 to_install=(
-	neovim
-	google-chrome-stable	
-	xclip
-	akmod-nvidia akmods kernel-devel kernel-headers # nvidia schenanigans
-	stow
-	zsh
+  neovim
+  google-chrome-stable
+  xclip
+  akmod-nvidia akmods kernel-devel kernel-headers # nvidia schenanigans
+  stow
+  zsh
+  wezterm
 )
+
+repo_to_activate=(
+  wezfurlong/wezterm-nightly
+)
+# Enable COPR repos if not already enabled
+for repo in "${repo_to_activate[@]}"; do
+  if ! sudo dnf repolist enabled | grep -q "^${repo}"; then
+    echo "➕ Enabling COPR repo: $repo"
+    sudo dnf copr enable -y "$repo"
+  else
+    echo "✔ COPR repo $repo already enabled"
+  fi
+done
+
+# Install packages
 for pkg in "${to_install[@]}"; do
-	if rpm -q "$pkg" &> /dev/null; then
-		echo "✔ $pkg is already installed"
-	else		
-		echo "➕ Installing $pkg ..."
-		sudo dnf install -y "$pkg" 
-	fi
+  if rpm -q "$pkg" &>/dev/null; then
+    echo "✔ $pkg is already installed"
+  else
+    echo "➕ Installing $pkg ..."
+    sudo dnf install -y "$pkg"
+  fi
 done
