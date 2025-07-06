6722d0af9a698fdd35d673cfcfc03faf32f4f7b1
Adrian <adrian.kuhn97@gmail.com>
2025-07-06 16:18:57 +0200

    complete toolbox install script

--- jetbrains-toolbox.bash
+++ jetbrains-toolbox.bash
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-
+wget -O ~/downloads/toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.6.3.43718.tar.gz
+cd ~/downloads/
+tar -xzf toolbox.tar.gz --one-top-level=toolbox --strip-components=1
+rm toolbox.tar.gz
+mkdir -p ~/jetbrains/
+mv toolbox ~/jetbrains/
