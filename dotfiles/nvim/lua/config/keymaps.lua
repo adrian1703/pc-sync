534bb48ffb76f4de699dd046e8466084a8d9d259
Adrian <adrian.kuhn97@gmail.com>
2025-07-06 16:19:14 +0200

    add useful keybinds

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -6,6 +6,10 @@ vim.api.nvim_set_keymap("n", ";", ":", { noremap = false })
 vim.api.nvim_set_keymap("n", "XX", ":wq<Enter>", { noremap = false })
 vim.api.nvim_set_keymap("n", "SS", ":wa<Enter>", { noremap = false })
 
+vim.api.nvim_set_keymap("t", "<C-Space>", [[<C-\><C-n>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>t", ":split | terminal<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>T", ":vsplit | terminal<CR>", { noremap = true, silent = true })
+
 vim.api.nvim_set_keymap("n", "tk", ":blast<enter>", { noremap = false })
 vim.api.nvim_set_keymap("n", "tj", ":bfirst<enter>", { noremap = false })
 vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", { noremap = false })
