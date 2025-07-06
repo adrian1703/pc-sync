b42bc7b4922d614a4d0593424d1a36d76278465f
Adrian <adrian.kuhn97@gmail.com>
2025-07-06 14:00:03 +0200

    keymap adjustments

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -2,11 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "k(", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("n", ";", ":", { noremap = false })
 vim.api.nvim_set_keymap("n", "XX", ":wq<Enter>", { noremap = false })
 vim.api.nvim_set_keymap("n", "SS", ":wa<Enter>", { noremap = false })
 
 vim.api.nvim_set_keymap("n", "tk", ":blast<enter>", { noremap = false })
 vim.api.nvim_set_keymap("n", "tj", ":bfirst<enter>", { noremap = false })
-vim.api.nvim_set_keymap("n", "th", ":bprev<enter>", { noremap = false })
-vim.api.nvim_set_keymap("n", "tl", ":bnext<enter>", { noremap = false })
 vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", { noremap = false })
