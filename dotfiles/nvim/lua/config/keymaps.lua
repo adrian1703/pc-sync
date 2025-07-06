bc32cb75fe6fa6ac789844d4410804b5d3318f8b
Adrian <adrian.kuhn97@gmail.com>
2025-07-06 13:13:27 +0200

    added some custom keybinds

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.api.nvim_set_keymap("i", "k(", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("n", "XX", ":wq<Enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "SS", ":wa<Enter>", { noremap = false })
+
+vim.api.nvim_set_keymap("n", "tk", ":blast<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "tj", ":bfirst<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "th", ":bprev<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "tl", ":bnext<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", { noremap = false })
