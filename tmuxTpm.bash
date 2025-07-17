3345daa4f4b81b38d43d89f2a4d187fca7db4325
Adrian <adrian.kuhn@arcor.de>
2025-07-17 21:03:02 +0200

    . filename adjustment

--- tmuxTpm.bash
+++ tmuxTpm.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DEST="$HOME/.config/.tmux/plugins/"
-rm -r "$DEST/tmp" &>/dev/null
+DEST="$HOME/.config/tmux/plugins/tpm"
+rm -r "$DEST"
 mkdir -p "$DEST"
 git clone https://github.com/tmux-plugins/tpm "$DEST"
