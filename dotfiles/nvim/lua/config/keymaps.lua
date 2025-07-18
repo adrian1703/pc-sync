b38aa43902736b56d5d86a139ea287b0945be490
Adrian <adrian.kuhn@arcor.de>
2025-07-19 00:06:10 +0200

    change cmd prompt

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -1,9 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.api.nvim_set_keymap("i", "k(", "<Esc>", { noremap = false })
+-- vim.api.nvim_set_keymap("i", "k(", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = false })
-vim.api.nvim_set_keymap("n", "XX", ":wq<Enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "XX", ":qa<Enter>", { noremap = false })
 vim.api.nvim_set_keymap("n", "SS", ":wa<Enter>", { noremap = false })
 
 vim.api.nvim_set_keymap("t", "<C-Space>", [[<C-\><C-n>]], { noremap = true, silent = true })
