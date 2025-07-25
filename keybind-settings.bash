f3962fcff7799074182ef2321747e3d26531afb3
Adrian <adrian.kuhn@arcor.de>
2025-07-25 15:45:46 +0200

    add workspace keybinds

---
+++ keybind-settings.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>Page_Down']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>Page_Up']"
