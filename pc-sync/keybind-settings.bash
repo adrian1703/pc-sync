0cda625a80f0bf08ddf1e0287f4af06a9da3429e
Adrian <adrian.kuhn@arcor.de>
2025-08-22 08:18:22 +0200

    move script

--- pc-sync/keybind-settings.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Super><Shift>Page_Down']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Super><Shift>Page_Up']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>Page_Down']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>Page_Up']"
